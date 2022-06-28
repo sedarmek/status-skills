1a449ab88b3c922cf306737dc6c658e88e0eb263
Sergio <sdajinvers@outlook.com>
2022-06-28 11:34:20 -0500

    se agrega funcionalidad para que se guarden las notas en storage

--- js/themes.js
+++ js/themes.js
@@ -3,7 +3,14 @@ window.onload = () => {
 
     let btnThemeDark = document.querySelector('.theme-dark');
     let btnThemeLight = document.querySelector('.theme-light');
-    
+    let notesSkill = document.querySelector('.article-notes');
+    let btnNotesSkill = document.querySelector('.notes-button')
+    notesSkill.value = localStorage.getItem('storageNotesSkill')
+
+    btnNotesSkill.addEventListener('click',()=>{
+        localStorage.setItem('storageNotesSkill', notesSkill.value)
+    })
+
     btnThemeDark.addEventListener('click', ()=> {
         document.querySelector('.theme-loaded').setAttribute('href', '../css/themes/dark-theme.css');
         btnThemeLight.style.display = "block";
